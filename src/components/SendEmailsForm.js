ab265740a15151d6c02afd993434e0010a6b1cc8
dbads <deepakbharti@mnnit.ac.in>
2024-05-09 10:24:55 +0530

    added cc and bcc emails

--- src/components/SendEmailsForm.js
+++ src/components/SendEmailsForm.js
@@ -4,7 +4,11 @@ import '../css/CreateFoundationForm.css'
 
 const SendEmailsForm = () => {
   const [formData, setFormData] = useState(
-    []
+    {
+      emails: '',
+      ccEmail: '',
+      bccEmail: '',
+    }
   )
 
   const [successMessage, setSuccessMessage] = useState('');
@@ -13,7 +17,14 @@ const SendEmailsForm = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     console.log(name, value);
-    setFormData([...value.split(",")]);
+    if (name === 'emails') {
+      const custom_emails = value.split(",");
+      setFormData({ ...formData, [name]: value , emails: custom_emails});
+    } else {
+      const custom_emails = formData['emails'].split(",");
+      setFormData({ ...formData, [name]: value , emails: custom_emails});
+    }
+    
   };
 
   const handleSubmit = (e) => {
@@ -21,7 +32,11 @@ const SendEmailsForm = () => {
     axios.post('http://localhost:8080/nonprofit/sendEmails', formData)
       .then(response => {
         console.log(response.data);
-        setFormData([])
+        setFormData({
+          emails: '',
+          ccEmail: '',
+          bccEmail: ''
+        });
         
         setSuccessMessage('Emails sent to non profits successfully!');
 
@@ -41,7 +56,17 @@ const SendEmailsForm = () => {
       <form className="form" onSubmit={handleSubmit}>
         <label>
           Enter the email of non profits comma seperated:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+          <input type="text" name="emails" value={formData.name} onChange={handleChange} />
+        </label>
+        
+        <label>
+          Enter the cc email
+          <input type="text" name="ccEmail" value={formData.name} onChange={handleChange} />
+        </label>
+        
+        <label>
+          Enter the bcc Email
+          <input type="text" name="bccEmail" value={formData.name} onChange={handleChange} />
         </label>
 
         <button type="submit">Submit</button>
